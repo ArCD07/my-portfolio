614c6cc6f374d727aeb8d1956f42fa66be8cee22
ArCD07 <armandoclemente1407@gmail.com>
2025-01-26 21:18:37 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("boton-saludo").addEventListener("click", () => {
+    const mensaje = document.getElementById("mensaje");
+    mensaje.textContent = "¡Hola! Gracias por visitar mi proyecto.";
+    mensaje.style.color = "green";
+  });
+  
